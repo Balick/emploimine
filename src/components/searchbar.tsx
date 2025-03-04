aaf1b62906842acd03a4e0a5a7264daffd55b83b
Théo Balick <balickmethens@gmail.com>
2025-03-04 13:21:11 +0100

    Améliorer l'expérience de recherche avec un défilement automatique
    - Ajouter une fonction scrollToSearch pour centrer le formulaire de recherche
    - Implémenter un useEffect pour défiler automatiquement lors d'une recherche existante
    - Ajouter un gestionnaire onFocus pour un défilement fluide lors de l'interaction

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -2,6 +2,7 @@
 
 import { Search } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { Input } from "./ui/input";
 
@@ -20,10 +21,24 @@ export default function SearchBar() {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
+  const scrollToSearch = () => {
+    document.getElementById("form")?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  useEffect(() => {
+    if (searchParams.has("query")) {
+      scrollToSearch();
+    }
+  }, []);
+
   return (
     <form id="form" className="h-[48px] relative">
       <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground w-4 h-4" />
       <Input
+        onFocus={scrollToSearch}
         defaultValue={searchParams.get("query")?.toString()}
         onChange={(e) => handleSearch(e.target.value)}
         placeholder="Chercher par emploi, entreprise..."
