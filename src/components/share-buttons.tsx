c0f559c120e83066283d5fda589579481dbeae4c
Théo Balick <balickmethens@gmail.com>
2025-03-23 23:24:20 +0100

    Redesigned sharing buttons for a better user experience
    - Added Facebook and X sharing functionality with clickable buttons
    - Improved copy-paste management with reduced delay for copy indicator
    - Code reorganization for improved readability and removal of unnecessary links

--- src/components/share-buttons.tsx
+++ src/components/share-buttons.tsx
@@ -1,37 +1,59 @@
 "use client";
 
+import { ClipboardCheck, Link } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
-import { Link, ClipboardCheck } from "lucide-react";
-import Image from "next/image";
 
 export default function ShareButtons() {
   const url = window.location.href;
   const [isCopied, setIsCopied] = React.useState(false);
+
+  const handleShare = (platform: string) => {
+    let shareUrl = "";
+    const encodedUrl = encodeURIComponent(url);
+    const windowFeatures =
+      "width=600,height=400,menubar=no,toolbar=no,location=no,resizable=yes,scrollbars=yes,status=no";
+
+    switch (platform) {
+      case "facebook":
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
+        break;
+      case "x":
+        shareUrl = `https://x.com/intent/tweet?url=${encodedUrl}`;
+        break;
+      default:
+        return;
+    }
+
+    window.open(shareUrl, "_blank", windowFeatures);
+  };
 
   const handleCopy = () => {
     navigator.clipboard.writeText(url);
     setIsCopied(true);
     setTimeout(() => {
       setIsCopied(false);
-    }, 10000);
+    }, 3000);
   };
 
   return (
     <div className="flex mt-[20px] gap-4">
-      <a href="#" target="_blank" rel="noreferrer">
-        <Button className="flex items-center gap-1.5 rounded-full border-[1.5px] font-semibold whitespace-nowrap border-[#e7e7e9] bg-white text-black hover:bg-white text-[13px] h-[40px] active:scale-105 transition-all duration-300">
-          <Image src={"/facebook.png"} alt="Facebook" width={16} height={16} />
-          <span>Partager</span>
-        </Button>
-      </a>
+      <Button
+        onClick={() => handleShare("facebook")}
+        className="flex items-center gap-1.5 rounded-full border-[1.5px] font-semibold whitespace-nowrap border-[#e7e7e9] bg-white text-black hover:bg-white text-[13px] h-[40px] active:scale-105 transition-all duration-300"
+      >
+        <Image src={"/facebook.png"} alt="Facebook" width={16} height={16} />
+        <span>Partager</span>
+      </Button>
 
-      <a href="#" target="_blank" rel="noreferrer">
-        <Button className="flex items-center gap-1.5 rounded-full border-[1.5px] font-semibold whitespace-nowrap border-[#e7e7e9] bg-white text-black hover:bg-white text-[13px] h-[40px] active:scale-105 transition-all duration-300">
-          <Image src={"/x.png"} alt="x" width={16} height={16} />
-          <span>Partager</span>
-        </Button>
-      </a>
+      <Button
+        onClick={() => handleShare("x")}
+        className="flex items-center gap-1.5 rounded-full border-[1.5px] font-semibold whitespace-nowrap border-[#e7e7e9] bg-white text-black hover:bg-white text-[13px] h-[40px] active:scale-105 transition-all duration-300"
+      >
+        <Image src={"/x.png"} alt="X" width={16} height={16} />
+        <span>Partager</span>
+      </Button>
 
       <Button
         onClick={handleCopy}
